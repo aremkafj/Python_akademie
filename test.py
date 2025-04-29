3f05250399367cdef3d7dbe465d53b2b03134cb5
aremkafj <juhanakova.felicita@gmail.com>
2025-04-29 13:42:55 +0200

    Přidaní nového řádku - zkouška

---
+++ test.py
@@ -0,0 +1,18 @@
+print("Ahoj, jmenuji se Felicita, ale říkají mi ‘Feli‘")
+# Můj komentář
+# je
+# je
+
+print("Učím se python")
+print("funkce print začína slovem print")
+print('funkci print zavoláme ("print")')
+
+print("Ahoj, \nmé jméno je Felicita. \nJe mi 32 let")
+
+# Naučila jsem se funkci print a složité/jednoduché uvozovky
+# Dále jsem se naučila funkci \n, kdy se jedna věta rozloží do 
+# libovolného počtu řádků
+
+print("Ahoj")
+
+# Zkouška změny souboru
